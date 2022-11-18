655609c39817ad3604cfb1c756c398eb30951c7c
Gines Martinez <ginesmartinezgracia@gmail.com>
2022-11-18 16:43:49 +0100

    README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pairPrograming
+## PARTICIPANTES
+Ginés Martínez Gracia - Gines.Martinez@solera.com
+Jorge Acevedo de León - Jorge.Acevedo@solera.com
--- Tennis/src/tennis/Tennis.java
+++ Tennis/src/tennis/Tennis.java
@@ -1,5 +1,12 @@
 package tennis;
 
 public class Tennis {
+	private int server = 0;
+	private int receiver = 0;
+	
+	public static void main(String []args) {
+		System.out.println("Let the tennis game begin! ");
+		System.out.println("Actual score  ");
 
+	}
 }
